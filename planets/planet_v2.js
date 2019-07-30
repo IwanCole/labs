674581d173234d03f6e0b8ddbfae3a57ed9186c3
IwanCole <iwan.cole.2015@bristol.ac.uk>
2019-07-30 21:44:57 +0100

    Added mobile scaling on tap, desktop clock, and helper popup

--- planets/planet_v2.js
+++ planets/planet_v2.js
@@ -1,12 +1,19 @@
-var WIDTH = null;
-var HEIGHT = null;
-var X0 = null;
-var Y0 = null;
-var dampener = 0.1;
-var hyper = false;
+var X0 = null;          // The origin X co-ord
+var Y0 = null;          // The origin Y co-ord
+var WIDTH = null;       // Global width of the canvas
+var HEIGHT = null;      // Global height of the canvas
+var hyper = false;      // Boost factor for oribital speed
+var dampener = 0.1;     // Dampening factor of orbital speed
+var MAXSPEED = null;    // Max orbital speed
+var MINSPEED = null;    // Min orbital speed
+var planets = [];       // Planets and their moons
+var tapStart = null;    // (mobile) Time of touch start
+var mobileZoom = false; // (mobile) Viewport scaling active or not
+
+const MOBILE = mobileCheck();
+
 var canvas = document.querySelector("canvas");
 var ctx = canvas.getContext('2d');
-var planets = [];
 
 
 class Planet {
@@ -43,6 +50,7 @@ class Planet {
 }
 
 
+// Includes a 'planet index' plt_i referencing which planet-object the moon belongs to
 class Moon extends Planet {
     constructor(x0, y0, orbit, radius, colour, speed, plt_i) {
         const x = planets[plt_i].x;
@@ -59,6 +67,8 @@ class Moon extends Planet {
 }
 
 
+// Calculates the size of the webpage, adjusts the canvas to fit.
+// Sets the origin (sun) co-ords X0,Y0.
 function calculateSize() {
     if ( navigator.platform != "iPad" && navigator.platform != "iPhone" && navigator.platform != "iPod" ) {
         HEIGHT = innerHeight;
@@ -67,24 +77,105 @@ function calculateSize() {
         HEIGHT = screen.height;
         WIDTH = screen.width;
     }
+    
+    WIDTH *= (mobileZoom) ? 2 : 1;
+    HEIGHT *= (mobileZoom) ? 2 : 1;
+    
     canvas.width = WIDTH;
     canvas.height = HEIGHT;
     X0 = Math.round(WIDTH/2);
     Y0 = Math.round(HEIGHT/2);
 }
 
+
+// Scales the orbits of the planets on page load to fit the screen. Ensures Pluto remains on screen.
+// On mobile, adjusts radii of planets to be more visible, and hides most outer planets.
 function scaleSize() {
-
-    // Scale max orbit (pluto) to size of screen's smallest dimention - no clipping
-    var sizeScale = ((WIDTH < HEIGHT) ? WIDTH : HEIGHT) / (2*PLT.pl.orbit);
-    for (var i = 0; i < planets.length; i++) planets[i].orbit *= sizeScale;
-
-    // Scale bodies if page is too small
-    if (HEIGHT < 750 || WIDTH < 750) {
-        for (var i = 0; i < planets.length; i++) planets[i].orbit *= 1.1;
-        for (var i = 0; i < planets.length; i++) planets[i].radius *= 0.6;
+    if (!MOBILE) {
+        var sizeScale = ((WIDTH < HEIGHT) ? WIDTH : HEIGHT) / (2*PLT.pl.orbit);
+        for (var i = 0; i < planets.length; i++) planets[i].orbit *= sizeScale;
+        if (HEIGHT < 750 || WIDTH < 750) {
+            for (var i = 0; i < planets.length; i++) planets[i].orbit *= 1.1;
+            for (var i = 0; i < planets.length; i++) planets[i].radius *= 0.6;
+        }
+    }
+    else {
+        for (var i = 0; i < 9; i++) planets[i].orbit *= 1.5;
+        for (var i = 9; i < planets.length; i++) planets[i].orbit *= 1.6;
+        for (var i = 0; i < planets.length; i++) planets[i].radius *= 2;
     }
 }
+
+
+// Re-centers all the planets (not moons) to have the origin co-ords.
+function recenter() {
+    for (var i = 0; i < 9; i++) planets[i].origin = { x: X0, y: Y0 };
+}
+
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    h = (h < 10) ? `0${h}` : h;
+    m = (m < 10) ? `0${m}` : m;
+    document.querySelector(".clock").innerHTML = `${h}:${m}`;
+    var t = setTimeout(startTime, 30000);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// Adds page event handlers. On mobile, a short tap re-scales the viewport to view all the outer planets.
+function handlers() {
+    var rocket = document.querySelector(".info-pic");
+    rocket.addEventListener('click', (e) => {
+        var notif = document.querySelector(".info");
+        notif.classList.add("info-active");
+        rocket.remove();
+        
+    });
+    
+    if (!MOBILE) {
+        canvas.addEventListener('mousedown', (e) => { hyper = true; });
+        canvas.addEventListener('mouseup', (e) => { hyper = false; });
+        window.addEventListener('resize', (e) => {
+            calculateSize();
+            recenter();
+        });
+        window.addEventListener('keydown', (e) => {
+            if (e.keyCode && e.keyCode == 32) {
+                document.querySelector(".clock").classList.toggle("clock-active");
+            } 
+        });
+    }
+    else {
+        canvas.addEventListener('touchstart', (e) => { 
+            hyper = true;
+            d = new Date();
+            tapStart = d.getTime();
+        });
+        canvas.addEventListener('touchend', (e) => { 
+            hyper = false;
+            d = new Date();
+            if (d.getTime() - tapStart < 100) {
+                mobileZoom = !mobileZoom
+                document.querySelector('canvas').classList.toggle("canvas-zoom");
+                calculateSize();
+                recenter();
+            }
+        });
+    }
+}
+
 
 function animate() {
     req = requestAnimationFrame(animate);
@@ -97,36 +188,44 @@ function animate() {
 
     for (var i = 0; i < planets.length; i++) planets[i].update();
 
-    if (hyper && dampener < 2) {
+    if (hyper && dampener < MAXSPEED) {
         dampener += 0.02;
-        if (dampener > 2) dampener = 2;
+        if (dampener > MAXSPEED) dampener = MAXSPEED;
     }
-    else if (!hyper && dampener > 0.15) {
+    else if (!hyper && dampener > MINSPEED) {
         dampener -= 0.02;
-        if (dampener < 0.15) dampener = 0.15;
+        if (dampener < MINSPEED) dampener = MINSPEED;
     }
 }
 
 
 function init() {
     calculateSize();
+
     Object.keys(PLT).forEach(function(key,index) {
         planets.push(new Planet(X0, Y0, PLT[key].orbit, PLT[key].radius, PLT[key].col, PLT[key].speed));
     });
     Object.keys(MOON).forEach(function(key,index) {
         planets.push(new Moon(0, 0, MOON[key].orbit, MOON[key].radius, MOON[key].col, MOON[key].speed, MOON[key].plt_i));
     });
-    scaleSize();
 
-    window.addEventListener('mousedown', (e) => { hyper = true; });
-    window.addEventListener('mouseup', (e) => { hyper = false; });
-    window.addEventListener('touchstart', (e) => { hyper = true; });
-    window.addEventListener('touchend', (e) => { hyper = false; });
-    window.addEventListener('resize', (e) => {
-        calculateSize();
-        for (var i = 0; i < 9; i++) planets[i].origin = { x: X0, y: Y0 };
-        // scaleSize();
-    });
+    scaleSize();
+    handlers();
+    
+    startTime();
+    
+    var t1 = document.querySelector(".text1");
+    var t2 = document.querySelector(".text2");
+    if (MOBILE) {
+        MINSPEED = 0.35;
+        MAXSPEED = 3.5;
+        t2.textContent = "Tap to zoom in and out"
+    }
+    else {
+        MINSPEED = 0.15;
+        MAXSPEED = 2;
+        t1.textContent = t1.textContent.replace("Tap", "Click");
+    }
 }
 
 
